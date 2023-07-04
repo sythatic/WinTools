9ae0c9eb70e0ebbd7f38a350e459541f18a4394a
Dom Panaia <dompanaia@gmail.com>
2023-07-04 16:40:40 -0400

    Update and rename wintools-v0.2.2.cmd to wintools-v0.2.3.cmd

--- source/wintools-v0.2.3.cmd
+++ source/wintools-v0.2.3.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-REM Check if the user is an administrator
 net session >nul 2>&1
 if %errorLevel% equ 0 (
     goto top
@@ -15,7 +14,7 @@ if %errorLevel% equ 0 (
 :top
 echo:
 echo   [104m WinTools [0m
-echo   [90mv0.2.2 [0m
+echo   [90mv0.2.3 [0m
 echo:
 :prompt
 set "modify="
@@ -25,10 +24,8 @@ echo   [7m Applet Commands [0m
 :help
 echo:
 echo   [97mClear Icon Cache                            [93m[clrico]
-echo   [97mContext Menu Editor                         [91m[cmedit]
 echo   [97mDeployment Image Servicing and Management   [93m[dism]
 echo   [97mSuspend File Explorer                       [93m[killfe]
-echo   [97mChange Program Files Location               [91m[pgrmdir]
 echo   [97mGet OEM Product Key                         [93m[prodkey]
 echo   [97mSystem File Checker                         [93m[sfc]
 echo   [97mSet System Information                      [93m[sysinfo]
@@ -55,22 +52,6 @@ if /i "%modify%"=="clrico" (
     echo:
     goto prompt
 )
-if /i "%modify%"=="cmedit" (
-    echo:
-    echo   [7m Enter A File Extension [0m
-    echo:
-    echo   [43m This Applet is currently disabled and will not do anything [0m
-    echo:
-    set /p ext="[0m  $: ."
-    echo:
-    taskkill /f /im explorer.exe
-    echo:
-    echo reg add "HKEY_CLASSES_ROOT\.%ext%\ShellNew" /v "NullFile" /t REG_SZ /d "1" /f
-    echo:
-    start explorer.exe
-    echo:
-    goto prompt
-)
 if /i "%modify%"=="dism" (
     echo:
     dism /Online /Cleanup-Image /RestoreHealth
@@ -84,27 +65,6 @@ if /i "%modify%"=="killfe" (
     taskkill /f /im explorer.exe
     echo:
     pause
-    echo:
-    start explorer.exe
-    echo:
-    goto prompt
-)
-if /i "%modify%"=="pgrmdir" (
-    echo:
-    echo   [7m Enter New Program Location [0m
-    echo:
-    echo   [43m This Applet is currently disabled and will not do anything [0m
-    echo:
-    set /p newpgrmdir="[0m  Path: "
-    echo:
-    taskkill /f /im explorer.exe
-    echo:
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "ProgramFilesDir" /t REG_SZ /d "%newpgrmdir%"
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "ProgramFilesDir (x86)" /t REG_SZ /d "%newpgrmdir% (x86)"
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "ProgramW6432Dir" /t REG_SZ /d "%newpgrmdir%"
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "CommonFilesDir" /t REG_SZ /d "%newpgrmdir%\Common Files"
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "CommonFilesDir (x86)" /t REG_SZ /d "%newpgrmdir% (x86)\Common Files"
-    echo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion" /f /v "CommonW6432Dir" /t REG_SZ /d "%newpgrmdir%\Common Files"
     echo:
     start explorer.exe
     echo:
@@ -148,11 +108,37 @@ if /i "%modify%"=="winpatch" (
     echo:
     taskkill /f /im explorer.exe
     echo:
-    reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
-    reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /v "" /t REG_SZ /d ""
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\PCHC" /v "PreviousUninstall" /t REG_DWORD /d 1 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\PCHealthCheck" /v "installed" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableBlurBehind" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "HideSCAMeetNow" /t REG_DWORD /d 1 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "HideSCAMeetNow" /t REG_DWORD /d 1 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Explorer" /v "HidePeopleBar" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v "HidePeopleBar" /t REG_DWORD /d 1 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v "PeopleBand" /t REG_DWORD /d 0 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Windows Feeds" /v "EnableFeeds" /t REG_DWORD /d 0 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Feeds" /v "ShellFeedsTaskbarViewMode" /t REG_DWORD /d 2 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Windows Search" /v "AllowCloudSearch" /t REG_DWORD /d 0 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d 0 /f
+    reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Windows Search" /v "AllowCortanaAboveLock" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v "CortanaEnabled" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v "CortanaConsent" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsSpotlightWindowsWelcomeExperience" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsSpotlightFeatures" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsSpotlightOnActionCenter" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsSpotlightOnSettings" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableThirdPartySuggestions" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "ConfigureWindowsSpotlight" /t REG_DWORD /d 2 /f
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\CloudContent" /v "IncludeEnterpriseSpotlight" /t REG_DWORD /d 0 /f
     reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /f /v "DisableSearchBoxSuggestions" /t REG_DWORD /d 1
     reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\SearchSettings" /f /v "IsDynamicSearchBoxEnabled" /t REG_DWORD /d 0
     reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /f /v "BingSearchEnabled" /t REG_DWORD /d 0
+    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "AllowOnlineTips" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /v "" /t REG_SZ /d ""
     reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Personalization" /f /v "NoLockScreen" /t REG_DWORD /d 1
     reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "verbosestatus" /t REG_DWORD /d 1
     reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /f /v "StartupDelayInMSec" /t REG_DWORD /d 0
@@ -169,6 +155,19 @@ if /i "%modify%"=="winpatch" (
     reg add "HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command" /f /v "" /t REG_SZ /d "%SystemRoot%\System32\rundll32.exe \"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll\", ImageView_PrintTo %1"
     reg add "HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\Move To" /f /ve /d "{C2FBB631-2971-11D1-A18C-00C04FD75D13}"
     reg add "HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\Copy To" /f /ve /d "{C2FBB630-2971-11D1-A18C-00C04FD75D13}"
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ServerAdminUI" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "DontPrettyPath" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "MapNetDrvBtn" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SeparateProcess" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AutoCheckSelect" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowStatusBar" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "UseCompactMode" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideMergeConflicts" /t REG_DWORD /d 0 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowEncryptCompressedColor" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "NavPaneShowAllCloudStates" /t REG_DWORD /d 1 /f
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideDrivesWithNoMedia" /t REG_DWORD /d 0 /f
     echo:
     start explorer.exe
     echo:
